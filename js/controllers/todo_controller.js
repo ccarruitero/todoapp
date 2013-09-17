bb5fe040cd663f72cb1d10a05167cd1000e27da6
Cesar Carruitero <cesar@mozilla.pe>
2013-09-16 23:04:33 -0500

    some cleanup and order files

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -15,23 +15,5 @@ Todos.TodoController = Ember.ObjectController.extend({
     doneEditing: function(){
       this.set('isEditing', false); 
     }
-    /*acceptChanges: function () {
-      this.set('isEditing', false);
-      this.get('model').save();
-    },
-
-    isCompleted: function(key, value){
-      var model = this.get('model');
-
-      if (value === undefined) {
-        // property being used as a getter
-        return model.get('isCompleted');
-      } else {
-        // property being used as  setter
-        model.set('isCompleted', value);
-        model.save();
-        return value;
-      }
-    }.property('model.isCompleted')*/
   }
 });
